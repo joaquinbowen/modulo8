ff28d5db37ab88a772826866d3366ff653e9a1c0
Clearminds Dev <bowenjoaquinsd@gmail.com>
2026-04-13 17:46:50 -0500

    Config .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,22 @@
+//docker compose -p dnm up
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
+
+console.log("clave secreta:", jwtSecret)
+console.log("PORT:", port)
 
 app.get('/', (req, res) => {
     res.send('Api Backend Funcionando')
 
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Servidor escuchando el puerto " + PORT);
+
+
+app.listen(port, () => {
+    console.log("Servidor escuchando el puerto " + port);
 })
